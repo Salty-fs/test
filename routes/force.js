79f8f3aee88a6f5c0ac520d0af47f16cc3187944
Salty-fs <1362516752@qq.com>
2021-11-07 14:21:43 +0800

    fsz

--- routes/force.js
+++ routes/force.js
@@ -2,31 +2,40 @@ var express = require('express');
 var router = express.Router();
 var URL = require('url');
 const fs = require('fs')
-//加载mysql模块
-// var mysql = require('mysql');
+// 加载mysql模块
+var mysql = require('mysql');
 
-// var config = require('../config/dbconfig');
+var config = require('../config/dbconfig');
 
-// var connection = mysql.createConnection(config);
+var connection = mysql.createConnection(config);
 
 //SQL语句
- var  sql = 'SELECT * FROM user';
-// var  addSql = 'INSERT INTO user(id,psword) VALUES(?,?)';
+//  var  sql = 'SELECT * FROM user';
+var  addSql = 'INSERT INTO test(data) VALUES(?)';
 
 router.post('/', function(req, res, next) {
 
     //解析请求参数
     // var params = URL.parse(req.url, true).query;
     console.log("@req",req.body)
+    try {
+      let data = JSON.stringify(req.body) +"\r\n"
+      fs.writeFileSync('./log.txt', data,{ flag: 'a+' }, (err) => {})
+      //file written successfully
+    } catch (err) {
+      console.error(err)
+    }
     // console.log("@res",res)
-    // var addSqlParams = [params.id];
-              try {
-                let data = JSON.stringify(req.body) +"\r\n"
-                fs.writeFileSync('./log.txt', data,{ flag: 'a+' }, (err) => {})
-                //file written successfully
-              } catch (err) {
-                console.error(err)
-              }
+    var addSqlParams = [req.body];
+
+    connection.query(addSql,addSqlParams,function (err, result) {
+        if(err){
+         console.log('[INSERT ERROR] - ',err.message);
+         return;
+        }
+        result.message='success'
+        console.log(result.message)             
+    });
     res.send(req.body)
 
     //解析请求参数
